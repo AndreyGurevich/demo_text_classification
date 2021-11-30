7e46dbbbe4a85ea14948cdf98dfe361a6456ee7a
Andrey Gurevich <amber4eg@gmail.com>
2021-11-30 14:53:30 +0300

    Optuna for Flair

--- text_classification.py
+++ text_classification.py
@@ -1,4 +1,4 @@
-from utils.helper import vanilla_tfidf, bigrams_tfidf, vanilla_flair, OptunaTFIDF
+from utils.helper import tfidf_and_sgd, vanilla_flair, OptunaTFIDF, OptunaFlair
 import pandas as pd
 from pathlib import Path
 from sklearn.preprocessing import LabelEncoder
@@ -6,10 +6,19 @@ from sklearn.model_selection import StratifiedKFold, train_test_split
 from numpy import mean, std
 from multiprocessing import freeze_support
 import optuna
+from argparse import ArgumentParser
 
 if __name__ == '__main__':
     freeze_support()
     le = LabelEncoder()
+    parser = ArgumentParser()
+    parser.add_argument("--frac", default=1.0, type=float, help="Share of dataset to use for Flair training")
+    parser.add_argument("--max_epochs", default=10, type=int, help="Max number of epochs to use for Flair training")
+    args = parser.parse_args()
+    frac = args.frac
+    max_epochs = args.max_epochs
+    assert 0 < frac <= 1, "frac must be between 0 and 1"
+    assert 0 < frac, "max_epochs must be greater than zero"
 
     df = pd.read_csv(Path(".") / "data" / "spam.csv",
                      names=["label", "text"],
@@ -17,7 +26,7 @@ if __name__ == '__main__':
                      usecols=[0, 1],  # we need only two columns
                      encoding="ISO-8859-1",
                      on_bad_lines="warn")
-    # print(df.head())
+
     print(df["label"].value_counts())  # Check class balance: 4825 vs 747. Not bad, but slightly unbalanced
 
     assert df.loc[0, "label"] == "ham", "Something wrong with the datasource"
@@ -28,8 +37,9 @@ if __name__ == '__main__':
     # Use KFold because dataset is small and Stratified because it's unbalanced
     skf = StratifiedKFold(n_splits=5)
     scores = {
-        "vanilla_tfidf_with_sgd": [],
-        "bigrams_tfidf": [],
+        "TFIDF with SGD, all parameter by default": [],
+        "TFIDF with SGD, tuned by intuition": [],
+        "TFIDF with SGD, tuned by optuna": [],
         "vanilla_flair": [],
     }
 
@@ -41,14 +51,16 @@ if __name__ == '__main__':
         y_train = df.loc[train_index, "target"]
         y_valid = df.loc[valid_index, "target"]
 
-        scores["vanilla_tfidf_with_sgd"].append(vanilla_tfidf(X_train, y_train, X_valid, y_valid))
-        scores["bigrams_tfidf"].append(bigrams_tfidf(X_train, y_train, X_valid, y_valid))
-
+        scores["TFIDF with SGD, all parameter by default"].append(tfidf_and_sgd(X_train, y_train, X_valid, y_valid))
+        scores["TFIDF with SGD, tuned by intuition"].append(
+            tfidf_and_sgd(X_train, y_train, X_valid, y_valid, 1, 2, 0.9, 4))
 
     # Let's do 60/20/20 splitting
     # We can use eval_on_train_fraction option of trainer.train() method, but will do splitting manually to save
     # compatibility with possible additional frameworks (still looks like a little bit overengineering, but let it be).
-    df = df.sample(frac=0.5)
+    if frac < 1:
+        df = df.sample(frac=frac)
+
     X_train, X_check, y_train, y_check = train_test_split(df["text"],
                                                           df["target"],
                                                           test_size=0.4,
@@ -61,12 +73,14 @@ if __name__ == '__main__':
                                                               random_state=42,
                                                               stratify=y_check
                                                               )
-
-    # vanilla_flair_score = vanilla_flair(X_train, y_train, X_valid, y_valid, X_holdout, y_holdout, 1)
+    # vanilla_flair_score = vanilla_flair(X_train, y_train, X_valid, y_valid, X_holdout, y_holdout, max_epochs=1)
     # scores["vanilla_flair"] = vanilla_flair_score
 
     study = optuna.create_study(direction="maximize")
-    study.optimize(OptunaTFIDF(X_train, y_train, X_valid, y_valid), n_trials=100)
+    # study.optimize(OptunaTFIDF(X_train, y_train, X_valid, y_valid), timeout=60)
+    study.optimize(OptunaFlair(X_train, y_train, X_valid, y_valid, X_holdout, y_holdout, max_epochs=max_epochs),
+                   timeout=None,
+                   n_trials=3)
     print(study.best_trial)
 
     for key, value in scores.items():
--- utils/helper.py
+++ utils/helper.py
@@ -1,5 +1,6 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import cross_val_score
 from sklearn.metrics import f1_score
 from pathlib import Path
 from flair.embeddings import WordEmbeddings, FlairEmbeddings, DocumentLSTMEmbeddings
@@ -16,10 +17,18 @@ document_embeddings = DocumentLSTMEmbeddings(word_embeddings,
                                              reproject_words_dimension=256)
 
 
-def vanilla_tfidf(x_train, y_train, x_valid, y_valid):
-    vectorizer = TfidfVectorizer(ngram_range=(1, 1),
-                                 max_df=1.0,
-                                 min_df=1,
+def tfidf_and_sgd(x_train,
+                  y_train,
+                  x_valid,
+                  y_valid,
+                  bigram_lower_bound=1,
+                  bigram_upper_bound=1,
+                  max_df=1.0,
+                  min_df=1
+                  ):
+    vectorizer = TfidfVectorizer(ngram_range=(bigram_lower_bound, bigram_upper_bound),
+                                 max_df=max_df,
+                                 min_df=min_df,
                                  )  # this is default parameters, just want them to be visible
     x_train_vectored = vectorizer.fit_transform(x_train)  # Fit on train data only
     x_valid_vectored = vectorizer.transform(x_valid)
@@ -27,19 +36,6 @@ def vanilla_tfidf(x_train, y_train, x_valid, y_valid):
     sgd.fit(x_train_vectored, y_train)
     score = f1_score(y_true=y_valid, y_pred=sgd.predict(x_valid_vectored))
 
-    return score
-
-
-def bigrams_tfidf(x_train, y_train, x_valid, y_valid):
-    vectorizer = TfidfVectorizer(ngram_range=(1, 2),
-                                 max_df=0.90,
-                                 min_df=4,
-                                 )
-    x_train_vectored = vectorizer.fit_transform(x_train)  # Fit on train data only
-    x_valid_vectored = vectorizer.transform(x_valid)
-    sgd = SGDClassifier(random_state=42)
-    sgd.fit(x_train_vectored, y_train)
-    score = f1_score(y_true=y_valid, y_pred=sgd.predict(x_valid_vectored))
     return score
 
 
@@ -76,14 +72,45 @@ def vanilla_flair(x_train, y_train, x_valid, y_valid, x_holdout, y_holdout, max_
                                 label_type="topic")
     trainer = ModelTrainer(classifier, corpus)
     score = trainer.train(tempdir,
-                  max_epochs=max_epochs,
-                  num_workers=4)
+                          max_epochs=max_epochs,
+                          num_workers=4)
     return score["test_score"]
+
+
+class OptunaFlair(object):
+    def __init__(self, x_train, y_train, x_valid, y_valid, x_holdout, y_holdout, max_epochs):
+        self.max_epochs = max_epochs
+        prepare_temp_files_for_flair(x_train, y_train, x_valid, y_valid, x_holdout, y_holdout)
+        self.corpus = ClassificationCorpus(tempdir,
+                                           test_file="test.txt",
+                                           dev_file="dev.txt",
+                                           train_file="train.txt",
+                                           label_type="topic",
+                                           )
+        self.classifier = TextClassifier(document_embeddings,
+                                         label_dictionary=self.corpus.make_label_dictionary(label_type="topic"),
+                                         multi_label=False,
+                                         label_type="topic")
+
+    def __call__(self, trial):
+        # Calculate an objective value by using the extra arguments.
+        mini_batch_size = trial.suggest_int("mini_batch_size", 24, 64)
+        anneal_factor = trial.suggest_float("anneal_factor", 0.25, 0.5)
+        learning_rate = trial.suggest_float("learning_rate", 0.05, 0.15)
+        trainer = ModelTrainer(self.classifier, self.corpus)
+        score = trainer.train(tempdir,
+                              max_epochs=self.max_epochs,
+                              num_workers=4,
+                              mini_batch_size=mini_batch_size,
+                              anneal_factor=anneal_factor,
+                              learning_rate=learning_rate,
+                              )
+
+        return score["test_score"]
 
 
 class OptunaTFIDF(object):
     def __init__(self, x_train, y_train, x_valid, y_valid):
-        print("y_train", y_train.shape)
         self.x_train = x_train
         self.x_valid = x_valid
         self.y_train = y_train
@@ -93,17 +120,17 @@ class OptunaTFIDF(object):
         # Calculate an objective value by using the extra arguments.
         bigram_lower_bound = trial.suggest_int("bigram_lower_bound", 1, 4)
         bigram_upper_bound = trial.suggest_int("bigram_upper_bound", bigram_lower_bound, 4)
-        l1_ratio = trial.suggest_float("l1_ratio", 0.05, 0.2)
+        max_df = trial.suggest_float("max_df", 0.95, 1.0)
+        min_df = trial.suggest_int("min_df", 1, 5)
 
         vectorizer = TfidfVectorizer(ngram_range=(bigram_lower_bound, bigram_upper_bound),
-                                     max_df=1.0,
-                                     min_df=1,
+                                     max_df=max_df,
+                                     min_df=min_df,
                                      )  # this is default parameters, just want them to be visible
         x_train_vectored = vectorizer.fit_transform(self.x_train)  # Fit on train data only
         x_valid_vectored = vectorizer.transform(self.x_valid)
         print(x_train_vectored.shape)
-        sgd = SGDClassifier(random_state=42,
-                            l1_ratio=l1_ratio)
+        sgd = SGDClassifier(random_state=42)
         sgd.fit(x_train_vectored, self.y_train)
         score = f1_score(y_true=self.y_valid, y_pred=sgd.predict(x_valid_vectored))
 
